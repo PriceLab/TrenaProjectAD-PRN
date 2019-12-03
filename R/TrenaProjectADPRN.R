aa8c0424bc28dbacddb4bf7f3659fbb88af18c95
paul-shannon <paul.thurmond.shannon@gmail.com>
2019-12-03 14:02:10 -0800

    changed package name again

--- R/TrenaProjectADPRN.R
+++ R/TrenaProjectADPRN.R
@@ -4,36 +4,36 @@
 #' @importFrom AnnotationDbi select
 #' @import org.Hs.eg.db
 #'
-#' @title TrenaProjectAD-PRN-class
+#' @title TrenaProjectADPRN-class
 #'
-#' @name TrenaProjectAD-PRN-class
-#' @rdname TrenaProjectAD-PRN-class
-#' @aliases TrenaProjectAD-PRN
-#' @exportClass TrenaProjectAD-PRN
+#' @name TrenaProjectADPRN-class
+#' @rdname TrenaProjectADPRN-class
+#' @aliases TrenaProjectADPRN
+#' @exportClass TrenaProjectADPRN
 #'
 
-.TrenaProjectAD-PRN <- setClass("TrenaProjectAD-PRN",
+.TrenaProjectADPRN <- setClass("TrenaProjectADPRN",
                                   contains="TrenaProjectHG38")
 
 #----------------------------------------------------------------------------------------------------
-#' Define an object of class TrenaProjectAD-PRN
+#' Define an object of class TrenaProjectADPRN
 #'
 #' @description
 #' Expression, variant and covariate data for the genes of interest (perhaps unbounded) for pre-term birth studies
 #'
-#' @rdname TrenaProjectAD-PRN-class
+#' @rdname TrenaProjectADPRN-class
 #'
 #' @export
 #'
-#' @return An object of the TrenaProjectAD-PRN class
+#' @return An object of the TrenaProjectADPRN class
 #'
 
-TrenaProjectAD-PRN <- function(quiet=TRUE)
+TrenaProjectADPRN <- function(quiet=TRUE)
 
 {
    genomeName <- "hg38"
 
-   directory <- system.file(package="TrenaProjectAD-PRN", "extdata", "geneSets")
+   directory <- system.file(package="TrenaProjectADPRN", "extdata", "geneSets")
    geneSet.files <- list.files(directory)
    geneSets <- list()
 
@@ -47,11 +47,11 @@ TrenaProjectAD-PRN <- function(quiet=TRUE)
    footprintDatabaseNames <- c("brain_hint_16",  "brain_hint_20", "brain_wellington_16", "brain_wellington_20")
    footprintDatabaseHost <- "khaleesi.systemsbiology.net"
    footprintDatabasePort <- 5432
-   dataDirectory <- system.file(package="TrenaProjectAD-PRN", "extdata")
+   dataDirectory <- system.file(package="TrenaProjectADPRN", "extdata")
 
    covariatesFile <- NA_character_;
 
-   .TrenaProjectAD-PRN(TrenaProjectHG38(projectName="TrenaProjectAD-PRN",
+   .TrenaProjectADPRN(TrenaProjectHG38(projectName="TrenaProjectADPRN",
                                        supportedGenes=geneSets[[1]],
                                        footprintDatabaseHost=footprintDatabaseHost,
                                        footprintDatabasePort=footprintDatabasePort,
@@ -60,5 +60,5 @@ TrenaProjectAD-PRN <- function(quiet=TRUE)
                                        quiet=quiet
                                        ))
 
-} # TrenaProjectAD-PRN, the constructor
+} # TrenaProjectADPRN, the constructor
 #----------------------------------------------------------------------------------------------------
--- makefile
+++ makefile
@@ -3,13 +3,13 @@ all:  docs install
 docs:
 	R -e "devtools::document()"
 build:
-	(cd ..; R CMD build --no-build-vignettes TrenaProjectAD-PRN)
+	(cd ..; R CMD build --no-build-vignettes TrenaProjectADPRN)
 
 install:
-	(cd ..; R CMD INSTALL --no-test-load TrenaProjectAD-PRN)
+	(cd ..; R CMD INSTALL --no-test-load TrenaProjectADPRN)
 
 check:
-	(cd ..; R CMD check `ls -t TrenaProjectAD-PRN) | head -1`)
+	(cd ..; R CMD check `ls -t TrenaProjectADPRN) | head -1`)
 
 test:
 	for x in inst/unitTests/test_*.R; do echo $$x; R -f $$x; done
